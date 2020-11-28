4b7128ca4bb9bb3d1fc94d38c7ed670e8e8c8d7f
Nikita Tarasov <nikitatarasov987@gmail.com>
2020-11-28 19:06:22 +0400

    Readme added.

--- src/validations.js
+++ src/validations.js
@@ -2,5 +2,12 @@ export default function validateAPI(response) {
     if(!response.kids) {
         response.kids = [];
     }
+    if(!response.id) {
+        response.id = getRandomInt(1000)
+    }
     return response;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
